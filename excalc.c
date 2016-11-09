711b132cb87cf76ac3584bbd03a28cac9f9fb3ea
Akihito Souza Cruz Miyakawa <aki.scm13@gmail.com>
2016-11-08 21:57:40 -0300

    add esboco das func push e pop.

--- excalc.c
+++ excalc.c
@@ -7,3 +7,49 @@
 
 
 #include <stdlib.h>
+#include <stdio.c>
+#include <math.c>
+
+#define MAX 80;
+
+int main()
+{
+    char o;
+
+    printf("\n\nEste programa simula uma calculadora polonesa. Para calcular digite os numeros um de cada vez e por fim o sinal('+','-','*','/') da operacao. \n")
+    
+    
+
+
+
+
+
+}
+ 
+int c=0;
+float x[MAX];
+
+void push(float u)
+{
+    if(c==MAX)          
+    {   printf("Capacidade de armazenamento da pilha excedida.");
+        return 0;
+    }
+    else
+        x[c]= u;
+    c++;
+}
+
+void pop()
+{
+    if(c==0)          
+    {   printf("A pilha esta vazia.");
+        return 0;
+    }
+    c--;
+}
+
+
+
+
+
